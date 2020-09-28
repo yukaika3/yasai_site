8d7a74e32881cc16d27da7c6e19ff6eeec5a1c29
yukaika3 <yukaikamaru3@gmail.com>
2020-09-28 23:21:05 +0900

    first commit

---
+++ resources/views/beginner.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>VegeVejiSearch</title>
+</head>
+<body>
+  <header>
+    <div id="home"></div>
+    <a href=""><img src="img/logo.png"title="home" alt="logo"></a>
+
+    <div class="menu">
+          <ul>
+            <li><a href="#home"  title="home"> <i class="fas fa-home"></i> Home</a></li>
+            <li><a href="#vege"  title="vege"><i class="fas fa-laptop-code"></i> 野菜一覧</a></li>
+            <li><a href="#cal" title="cal"><i class="fas fa-user"></i> カロリー計算</a></li>
+            <li><a href="#mail"  title="mail"><i class="far fa-envelope"></i> お問い合わせ</a></li>
+          </ul>
+     </div>
+
+
+  </header>
+
+  <p></p>
+</body>
+</html>
