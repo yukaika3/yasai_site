0d5ef2a33ed8e46a5593da183e41b777255e0646
yukaika3 <yukaikamaru3@gmail.com>
2020-10-15 03:27:06 +0900

    css

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -8,15 +8,14 @@
        <div class="mx-auto" style="max-width:1200px">
            <h1 style="color:#555555; text-align:center; font-size:1.2em; padding:24px 0px; font-weight:bold;">商品一覧</h1>
            <div class="">
-               <div class="d-flex flex-row flex-wrap">
+           @foreach($stocks as $stock)
+               <div class="d-flex flex-row flex-wrap"><br>
+                      <img src="assets/images/{{$stock->imgpath}}" alt="" class="incart" ><br>
+                      {{$stock->name}} 
+                      {{$stock->detail}} 
                 
-                   @foreach($stocks as $stock)
-                      {{$stock->name}} <br>
-                     
-                      <img src="assets/images/{{$stock->imgpath}}" alt="" class="incart" >
-                      
-                      {{$stock->detail}} <br>
                    @endforeach
+                <div>
                     {{$stocks->links()}} 
 
                    
