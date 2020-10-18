98a27b108dc2c0dff628d4e4cac9921f821309a1
yukaika3 <yukaikamaru3@gmail.com>
2020-10-19 03:06:56 +0900

    css編集

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -2,26 +2,31 @@
 @extends('layouts.app')
 
 @section('content')
-
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<title>yasai_site</title>
+<link href="{{asset('/assets/css/style.css')}}" rel="stylesheet">
+</head>
+<body>
 <div class="container-fluid">
-   <div class="">
-       <div class="mx-auto" style="max-width:1200px">
+       <div class="mx-auto">
            <h1 style="color:#555555; text-align:center; font-size:1.2em; padding:24px 0px; font-weight:bold;">商品一覧</h1>
-           <div class="">
+           <div class="d-flex">
            @foreach($stocks as $stock)
-               <div class="d-flex flex-row flex-wrap"><br>
-                      <img src="assets/images/{{$stock->imgpath}}" alt="" class="incart" ><br>
-                      {{$stock->name}} 
-                      {{$stock->detail}} 
-                
-                   @endforeach
+           <div class="yasai">
+            <img src="assets/images/{{$stock->imgpath}}" alt="" ><br>
+              {{$stock->name}} 
+              {{$stock->detail}} 
+            </div>
+            @endforeach
                 <div>
                     {{$stocks->links()}} 
 
                    
                </div>
-           </div>
-       </div>
    </div>
 </div>
+</body>
+</html>
 @endsection
