5f02f3c6a18698b2ab195f6026fd43fb41c66392
yukaika3 <yukaikamaru3@gmail.com>
2020-10-23 01:23:22 +0900

    css

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,24 @@
+
 @extends('layouts.app')
 
 @section('content')
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<link href="{{asset('/assets/css/home.css')}}" rel="stylesheet">
+</head>
+
 <div class="container">
+
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
+     </div>
+     <div class="veg">
+     <img src="assets/images/vege.jpg" alt="" >
+    <p>aaaaa</p>
             </div>
         </div>
     </div>
 </div>
+</body>
+</html>
 @endsection
