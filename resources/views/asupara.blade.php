7c7af817f92a085e5a289f3d3ce2f7c6b5c4dbeb
yukaika3 <yukaikamaru3@gmail.com>
2020-11-02 02:03:51 +0900

    ページ追加

---
+++ resources/views/asupara.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<meta charset="UTF-8">
+<html lang="ja">
+<head>
+<link href="{{asset('/assets/css/home.css')}}" rel="stylesheet">
+</head>
+
+<div class="container">
+     <div class="veg">
+     <img src="assets/images/asupara.jpg" alt="" >
+    <p>アスパラガス</p>
+
+    
+    </div>
+
+    
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
