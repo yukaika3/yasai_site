99bd74ab2587ef849cbbc5f093d4b5f7f5103153
yukaika3 <yukaikamaru3@gmail.com>
2020-11-09 01:49:34 +0900

    asupara

--- resources/views/asupara.blade.php
+++ resources/views/asupara.blade.php
@@ -12,14 +12,19 @@
 <p>アスパラガス</p>
      <div class="veg">
      <img src="assets/images/asupara.jpg" alt="asupara" align="top" >
-     
+     </div>
+        <div class="Ex">
+            <p>分類：葉茎類</p>
+            <p>カロリー:22cal</p>
+            <p>主な栄養素:オリゴ糖 カロテン ビタミンB1 ビタミンB2 ビタミンC ビタミンE カルシウム カリウム 食物繊維 
+                葉酸 アスパラギン ルチン</p>
+            <p>アスパラギン：アスパラガスから発見された栄養素</p>
+            <p>豆知識：ホワイトアスパラよりグリーンアスパラの方が栄養価が高い</p>
+            <p>旬：５〜６月</p>
+            <p></p>
 
-    
+        </div>
 
-    
-    </div>
-
-    
             </div>
         </div>
     </div>
