8698210aa6c2037f28a4a1f277509a5495b96500
yukaika3 <yukaikamaru3@gmail.com>
2020-11-15 22:31:46 +0900

    ページ追加

--- resources/views/kaiware.blade.php
+++ resources/views/kaiware.blade.php
@@ -11,15 +11,15 @@
 <div class="container">
 <p></p>
      <div class="veg">
-     <img src="assets/images/.jpg" alt="" align="top" >
+     <img src="assets/images/kaiware.jpg" alt="" align="top" >
      </div>
         <div class="Ex">
-            <p>分類：</p>
-            <p>カロリー:(100g)</p>
-            <p>主な栄養素:</p>
+            <p>分類：果菜類</p>
+            <p>カロリー:21cal(100g)</p>
+            <p>主な栄養素:ビタミンKやビタミンE、βカロテン</p>
             <p></p>
-            <p>豆知識：</p>
-            <p>旬：</p>
+            <p>豆知識：かいわれは年中栽培され、旬がない。大根の持っている栄養素を持っている</p>
+            <p>旬：なし</p>
             <p></p>
 
         </div>
