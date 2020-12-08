fef1e836e1f6cc6b88e393c01de51357c1a0f342
yukaika3 <yukaikamaru3@gmail.com>
2020-12-09 01:44:07 +0900

    微調整

--- resources/views/calorie.blade.php
+++ resources/views/calorie.blade.php
@@ -1,29 +1,27 @@
+@extends('layouts.app')
+
+@section('content')
 <!DOCTYPE html>
+<meta charset="UTF-8">
 <html lang="ja">
 <head>
-  <meta charset="UTF-8">
-  <title>VegeVejiSearch</title>
+<link href="{{asset('/assets/css/yasai.css')}}" rel="stylesheet">
 </head>
-<body>
-  <header>
-  <div id="home"></div>
-    <a href=""><img src="img/logo.png"title="home" alt="logo"></a>
 
-    <div class="menu">
-          <ul>
-            <li><a href="#home"  title="home"> <i class="fas fa-home"></i> Home</a></li>
-            <li><a href="#vege"  title="vege"><i class="fas fa-laptop-code"></i> 野菜一覧</a></li>
-            <li><a href="#cal" title="cal"><i class="fas fa-user"></i> カロリー計算</a></li>
-            <li><a href="#mail"  title="mail"><i class="far fa-envelope"></i> お問い合わせ</a></li>
-            <li><a href="#mail"  title="mail"><i class="far fa-envelope"></i> お問い合わせ</a></li>
-          </ul>
+<div class="container">
+<p></p>
+     <div class="veg">
+     
      </div>
-     
+        <div class="Ex">
+ 
 
-     <h1>aaaaaa</h1>
-     
-  </header>
+        </div>
 
-  <p></p>
+            </div>
+        </div>
+    </div>
+</div>
 </body>
 </html>
+@endsection
